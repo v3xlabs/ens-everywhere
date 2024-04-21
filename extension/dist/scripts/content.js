e90c258d063157ca6c1d138a293dbe0dcdaff807
Luc <luc@lucemans.nl>
2024-04-21 17:26:36 +0000

    Bump

--- extension/dist/scripts/content.js
+++ extension/dist/scripts/content.js
@@ -167,126 +167,52 @@
      *   where the [data-highlight-target] matches an address such as 0x225f137127d9067788314bc7fcc1f36746a3c3B5 (ethereum address regex)
      */
     function translateEtherscanAddresses2ENS() {
-        // Get all the address elements
-        const addressElements = document.querySelectorAll(
-            "td > div > span.hash-tag.text-truncate + a[data-clipboard-text]",
-        );
-
-        // Loop through them
-        addressElements.forEach((addressElement) => {
-            // Get the address
-            const address = addressElement.getAttribute("data-clipboard-text");
-
-            getName(address)
-                .then((data) => {
-                    // Get the ENS name
-                    const name = data["name"];
-
-                    // If there is no ENS name, return
-                    if (!name) {
-                        return;
-                    }
-
-                    // Replace the address with the ENS name
-                    let parent = addressElement.parentElement;
-                    parent.querySelector(
-                        "span.hash-tag.text-truncate",
-                    ).textContent = name;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        });
-
-        // Get all the address elements
-        const addressElements2 = document.querySelectorAll(
-            "td > div > a.hash-tag.text-truncate[href]",
-        );
-
-        // Loop through them
-        addressElements2.forEach((addressElement) => {
-            // Get the href
-            const href = addressElement.getAttribute("href");
-
-            // Get the address from the href
-            const address = href.split("/")[2];
-
-            // Fetch `https://enstate.rs/a/<address>`
-            getName(address)
-                .then((data) => {
-                    // Get the ENS name
-                    const name = data["name"];
-
-                    // If there is no ENS name, return
-                    if (!name) {
-                        return;
-                    }
-
-                    // Replace the address with the ENS name
-                    addressElement.textContent = name;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        });
-
-        // Strategy 3
-        // Get all the address elements
-        const addressElements3 = document.querySelectorAll(
-            "td > div > span[data-bs-title]",
-        );
-
-        // Loop through them
-        addressElements3.forEach((addressElement) => {
-            // Get the data-bs-title
-            const dataBsTitle = addressElement.getAttribute("data-bs-title");
-
-            // Get the address from the data-bs-title
-            const address = dataBsTitle;
-
-            // Fetch `https://enstate.rs/a/<address>`
-            getName(address)
-                .then((data) => {
-                    // Get the ENS name
-                    const name = data["name"];
-
-                    // If there is no ENS name, return
-                    if (!name) {
-                        return;
-                    }
-
-                    // Replace the address with the ENS name
-                    addressElement.textContent = name;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        });
-
-        // // Strategy 4
         // // Get all the address elements
-        // const addressElements4 = document.querySelectorAll(
-        //     ".hash-tag.text-truncate",
+        // const addressElements = document.querySelectorAll(
+        //     "td > div > span.hash-tag.text-truncate + a[data-clipboard-text]",
         // );
 
-        // console.log({ addressElements4 });
+        // // Loop through them
+        // addressElements.forEach((addressElement) => {
+        //     // Get the address
+        //     const address = addressElement.getAttribute("data-clipboard-text");
+
+        //     getName(address)
+        //         .then((data) => {
+        //             // Get the ENS name
+        //             const name = data["name"];
+
+        //             // If there is no ENS name, return
+        //             if (!name) {
+        //                 return;
+        //             }
+
+        //             // Replace the address with the ENS name
+        //             let parent = addressElement.parentElement;
+        //             parent.querySelector(
+        //                 "span.hash-tag.text-truncate",
+        //             ).textContent = name;
+        //         })
+        //         .catch((error) => {
+        //             console.log(error);
+        //         });
+        // });
+
+        // // Get all the address elements
+        // const addressElements2 = document.querySelectorAll(
+        //     "td > div > a.hash-tag.text-truncate[href]",
+        // );
 
         // // Loop through them
-        // addressElements4.forEach((addressElement) => {
-        //     // Get the text
-        //     const text = addressElement.textContent;
+        // addressElements2.forEach((addressElement) => {
+        //     // Get the href
+        //     const href = addressElement.getAttribute("href");
 
-        //     // If the text is not an ethereum address, return
-        //     if (!text.match(ethereum_address_regex)) {
-        //         return;
-        //     }
-
-        //     // Get the address from the text
-        //     const address = text;
+        //     // Get the address from the href
+        //     const address = href.split("/")[2];
 
         //     // Fetch `https://enstate.rs/a/<address>`
-        //     fetch(`https://enstate.rs/a/${address}`)
-        //         .then((response) => response.json())
+        //     getName(address)
         //         .then((data) => {
         //             // Get the ENS name
         //             const name = data["name"];
@@ -304,10 +230,84 @@
         //         });
         // });
 
+        // // Strategy 3
+        // // Get all the address elements
+        // const addressElements3 = document.querySelectorAll(
+        //     "td > div > span[data-bs-title]",
+        // );
+
+        // // Loop through them
+        // addressElements3.forEach((addressElement) => {
+        //     // Get the data-bs-title
+        //     const dataBsTitle = addressElement.getAttribute("data-bs-title");
+
+        //     // Get the address from the data-bs-title
+        //     const address = dataBsTitle;
+
+        //     // Fetch `https://enstate.rs/a/<address>`
+        //     getName(address)
+        //         .then((data) => {
+        //             // Get the ENS name
+        //             const name = data["name"];
+
+        //             // If there is no ENS name, return
+        //             if (!name) {
+        //                 return;
+        //             }
+
+        //             // Replace the address with the ENS name
+        //             addressElement.textContent = name;
+        //         })
+        //         .catch((error) => {
+        //             console.log(error);
+        //         });
+        // });
+
+        // // // Strategy 4
+        // // // Get all the address elements
+        // // const addressElements4 = document.querySelectorAll(
+        // //     ".hash-tag.text-truncate",
+        // // );
+
+        // // console.log({ addressElements4 });
+
+        // // // Loop through them
+        // // addressElements4.forEach((addressElement) => {
+        // //     // Get the text
+        // //     const text = addressElement.textContent;
+
+        // //     // If the text is not an ethereum address, return
+        // //     if (!text.match(ethereum_address_regex)) {
+        // //         return;
+        // //     }
+
+        // //     // Get the address from the text
+        // //     const address = text;
+
+        // //     // Fetch `https://enstate.rs/a/<address>`
+        // //     fetch(`https://enstate.rs/a/${address}`)
+        // //         .then((response) => response.json())
+        // //         .then((data) => {
+        // //             // Get the ENS name
+        // //             const name = data["name"];
+
+        // //             // If there is no ENS name, return
+        // //             if (!name) {
+        // //                 return;
+        // //             }
+
+        // //             // Replace the address with the ENS name
+        // //             addressElement.textContent = name;
+        // //         })
+        // //         .catch((error) => {
+        // //             console.log(error);
+        // //         });
+        // // });
+
         // Strategy 5
         // Get all the address elements
         const addressElements5 = document.querySelectorAll(
-            ".hash-tag.text-truncate > span[data-highlight-target]",
+            "span[data-highlight-target], a[data-highlight-target]",
         );
 
         console.log({ addressElements5 });
@@ -321,11 +321,20 @@
 
             // If the data-highlight-target is not an ethereum address, return
             if (!dataHighlightTarget.match(ethereum_address_regex)) {
+                console.log("Not updating because not ethereum address", dataHighlightTarget);
                 return;
             }
 
             // Get the address from the data-highlight-target
             const address = dataHighlightTarget;
+            address.slice(0, 8) + "..." + address.slice(-8);
+            address.slice(0, 8) + "..." + address.slice(-9);
+            addressElement.innerText;
+
+            // if ([address.toLowerCase(), truncatedAddress.toLowerCase(), truncatedAddress2.toLowerCase()].includes(bodyText.toLowerCase()) === false) {
+            //     console.log("Not updating because body mismatch");
+            //     return;
+            // }
 
             // Fetch `https://enstate.rs/a/<address>`
             getName(address)
